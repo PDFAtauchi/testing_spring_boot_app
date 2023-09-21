a69cdbc09c17f7d20119c707a09e86afdfd0d4e7
PDFAtauchi <floresatauchi@gmail.com>
2023-09-21 18:31:37 -0500

    feat: integration testing for layers

--- src/test/java/com/practice/springboottesting/integration/EmployeeControllerITests.java
+++ src/test/java/com/practice/springboottesting/integration/EmployeeControllerITests.java
@@ -1,45 +1,39 @@
-package com.practice.springboottesting.controller;
+package com.practice.springboottesting.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import com.practice.springboottesting.model.Employee;
-import com.practice.springboottesting.service.EmployeeService;
-
-import static org.hamcrest.CoreMatchers.is;
-
+import com.practice.springboottesting.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.ArgumentMatchers.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 
-@WebMvcTest
-public class EmployeeControllerTests {
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class EmployeeControllerITests {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -47,6 +41,12 @@ public class EmployeeControllerTests {
     private Employee employee;
 
     private String serviceUrl;
+
+    @BeforeEach
+    void setup() {
+        employee = getNewEmployee();
+        serviceUrl = "/api/employees/";
+    }
 
     Employee getNewEmployee() {
         Faker faker = new Faker(new Locale("en-US"));
@@ -65,20 +65,9 @@ public class EmployeeControllerTests {
         return employees;
     }
 
-    @BeforeEach
-    public void setup() {
-        employee = getNewEmployee();
-        serviceUrl = "/api/employees/";
-    }
-
     @Test
     public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
         // Given
-        given(employeeService.save(any(Employee.class)))
-                .willAnswer((invocation) -> {
-                    Employee employeeArgument = invocation.getArgument(0);
-                    return employeeArgument;
-                });
 
         // When
         ResultActions response = mockMvc.perform(post(serviceUrl)
@@ -98,8 +87,7 @@ public class EmployeeControllerTests {
         // Given
         int numberEmployees = 4;
         List<Employee> employees = getListOfNewEmployees(numberEmployees);
-
-        given(employeeService.getAllEmployees()).willReturn(employees);
+        employeeRepository.saveAll(employees);
 
         // When
         ResultActions response = mockMvc.perform(get(serviceUrl));
@@ -113,8 +101,7 @@ public class EmployeeControllerTests {
     @Test
     public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() throws Exception {
         // Given
-        employee.setId(1L);
-        given(employeeService.getEmployeeById(employee.getId())).willReturn(Optional.of(employee));
+        employeeRepository.save(employee);
 
         // When
         ResultActions response = mockMvc.perform(get(serviceUrl + "{id}/", employee.getId()));
@@ -130,11 +117,10 @@ public class EmployeeControllerTests {
     @Test
     public void givenInvalidEmployeeId_whenGetEmployeeById_thenReturnEmpty() throws Exception {
         // Given
-        employee.setId(1L);
-        given(employeeService.getEmployeeById(employee.getId())).willReturn(Optional.empty());
+        long employeeId = 1L;
 
         // When
-        ResultActions response = mockMvc.perform(get(serviceUrl + "{id}/", employee.getId()));
+        ResultActions response = mockMvc.perform(get(serviceUrl + "{id}/", employeeId));
 
         // Then
         response.andExpect(status().isNotFound())
@@ -144,19 +130,11 @@ public class EmployeeControllerTests {
     @Test
     public void givenUpdatedEmployee_whenUpdateEmployee_thenReturnUpdatedEmployeeObject() throws Exception {
         // Given
-        long employeeId = 1L;
-        employee.setId(employeeId);
-
+        employeeRepository.save(employee);
         Employee updatedEmployee = getNewEmployee();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.of(employee));
-        given(employeeService.updateEmployee(any(Employee.class)))
-                .willAnswer((invocation) -> {
-                    Employee employeeArgument = invocation.getArgument(0);
-                    return employeeArgument;
-                });
 
         // When
-        ResultActions response = mockMvc.perform(put(serviceUrl + "{id}/", employeeId)
+        ResultActions response = mockMvc.perform(put(serviceUrl + "{id}/", employee.getId())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(updatedEmployee)));
 
@@ -166,25 +144,17 @@ public class EmployeeControllerTests {
                 .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
                 .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
                 .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
-
     }
 
     @Test
     public void givenInvalidUpdatedEmployee_whenUpdateEmployee_thenReturn404() throws Exception {
         // Given
-        long employeeId = 1L;
-        employee.setId(employeeId);
-
+        long noExistEmployeeId = 2L;
+        employeeRepository.save(employee);
         Employee updatedEmployee = getNewEmployee();
-        given(employeeService.getEmployeeById(employeeId)).willReturn(Optional.empty());
-        given(employeeService.updateEmployee(any(Employee.class)))
-                .willAnswer((invocation) -> {
-                    Employee employeeArgument = invocation.getArgument(0);
-                    return employeeArgument;
-                });
 
         // When
-        ResultActions response = mockMvc.perform(put(serviceUrl + "{id}/", employeeId)
+        ResultActions response = mockMvc.perform(put(serviceUrl + "{id}/", noExistEmployeeId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(updatedEmployee)));
 
@@ -196,18 +166,13 @@ public class EmployeeControllerTests {
     @Test
     public void givenEmployeeId_whenDeleteEmployee_thenReturnStatusCode200() throws Exception {
         // Given
-        long employeeId = 1L;
-        employee.setId(employeeId);
-
-        willDoNothing().given(employeeService).deleteEmployee(employeeId);
+        employeeRepository.save(employee);
 
         // When
-        ResultActions response = mockMvc.perform(delete(serviceUrl + "{id}/", employeeId));
+        ResultActions response = mockMvc.perform(delete(serviceUrl + "{id}/", employee.getId()));
 
         // Then
         response.andExpect(status().isOk())
                 .andDo(print());
-        verify(employeeService, times(1)).deleteEmployee(employee.getId());
-
     }
 }
---
+++ src/test/java/com/practice/springboottesting/integration/EmployeeRepositoryITests.java
@@ -0,0 +1,174 @@
+package com.practice.springboottesting.integration;
+
+import com.practice.springboottesting.model.Employee;
+import com.practice.springboottesting.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class EmployeeRepositoryITests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Employee employee;
+
+    @BeforeEach
+    public void setup() {
+        employee = Employee.builder()
+                .firstName("Tom")
+                .lastName("lin")
+                .email("lin@gmail.com")
+                .build();
+    }
+
+    @Test
+    public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
+        // Given an employee
+
+        // When
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        // Then
+        assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee.getId()).isPositive();
+    }
+
+    @Test
+    public void givenEmployeesList_whenFindAll_thenEmployeesList() {
+        // Given
+        Employee employee1 = Employee.builder()
+                .firstName("Tom")
+                .lastName("lin")
+                .email("lin@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Tim")
+                .lastName("lang")
+                .email("lang@gmail.com")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        // When
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        // Then
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenEmployeeObject_whenFindById_thenReturnEmployeeObject() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findById(employee.getId()).get();
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findByEmail(employee.getEmail()).get();
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenEmployeeObject_whenUpdateEmployee_thenReturnUpdatedEmployee() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findById(employee.getId()).get();
+
+        String newEmail = "linupdate@gmail.com";
+        String newFirstName = "Lin";
+        retrievedEmployee.setEmail(newEmail);
+        retrievedEmployee.setFirstName(newFirstName);
+        Employee updatedEmployee = employeeRepository.save(retrievedEmployee);
+
+        // Then
+        assertThat(updatedEmployee.getEmail()).isEqualTo(newEmail);
+        assertThat(updatedEmployee.getFirstName()).isEqualTo(newFirstName);
+    }
+
+    @Test
+    public void givenEmployeeObject_whenDeleteEmployeeById_thenRemoveEmployee() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        employeeRepository.deleteById(employee.getId());
+        Optional<Employee> retrievedEmployee = employeeRepository.findById(employee.getId());
+
+        // Then
+        assertThat(retrievedEmployee).isEmpty();
+    }
+
+    @Test
+    public void givenFirstNameAndLastName_whenFindBy_thenReturnEmployeeObject() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findByFirstNameAndLastName(employee.getFirstName(), employee.getLastName());
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenFirstNameAndLastName_whenFindByParams_thenReturnEmployeeObject() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findByFirstNameAndLastNameParams(employee.getFirstName(), employee.getLastName());
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenFirstNameAndLastName_whenFindBySql_thenReturnEmployeeObject() {
+        // Given an employee
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findByFirstNameAndLastNameSql(employee.getFirstName(), employee.getLastName());
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+
+    @Test
+    public void givenFirstNameAndLastName_whenFindByParamsSql_thenReturnEmployeeObject() {
+        // Given
+        employeeRepository.save(employee);
+
+        // When
+        Employee retrievedEmployee = employeeRepository.findByFirstNameAndLastNameParamsSql(employee.getFirstName(), employee.getLastName());
+
+        // Then
+        assertThat(retrievedEmployee).isNotNull();
+    }
+}
